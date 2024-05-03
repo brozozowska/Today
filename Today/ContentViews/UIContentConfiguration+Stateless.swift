7184248cd7691696514f4b5470aa26c430818b4a
brozozowska <s_rozov@me.com>
2024-05-03 20:58:02 +0300

    Chapter 3. Using content views - complete.

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Сергей Розов on 03.05.2024.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
