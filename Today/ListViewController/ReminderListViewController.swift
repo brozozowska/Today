515dab5006404e482ab134d200765d046958b655
brozozowska <s_rozov@me.com>
2024-05-08 20:00:19 +0300

    Chapter 5. Creating a gradient background - complete.

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -67,6 +67,11 @@ class ReminderListViewController: UICollectionViewController {
         collectionView.dataSource = dataSource
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshBackground()
+    }
+    
     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         let id = filteredReminders[indexPath.item].id
         pushDetailViewForReminder(withID: id)
@@ -83,6 +88,14 @@ class ReminderListViewController: UICollectionViewController {
             return
         }
         progressView.progress = progress
+    }
+    
+    func refreshBackground() {
+        collectionView.backgroundColor = nil
+        let backgroundView = UIView()
+        let gradientLayer = CAGradientLayer.gradientLayer(for: listStyle, in: collectionView.frame)
+        backgroundView.layer.addSublayer(gradientLayer)
+        collectionView.backgroundView = backgroundView
     }
     
     func pushDetailViewForReminder(withID id: Reminder.ID) {
